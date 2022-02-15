b9c981ebe116befff2a03c3751f32ee9adbfc65b
Benito McClammy <BMCCLAMMY@augusta.edu>
2022-02-14 22:08:23 -0500

    adds db query to dbcon

--- app/src/usersearchlogic.php
+++ app/src/usersearchlogic.php
@@ -37,57 +37,16 @@ try {
 
 
     //determine account type
-    if ($acctype == "Student") {
-        //send back student type search results
-
-        $query = "SELECT * FROM User WHERE AccType=3 AND 
-            (Fname LIKE :fname OR :fname = 'defaultvalue!') AND
-            (Lname LIKE :lname OR :lname = 'defaultvalue!') AND
-            (DOB LIKE :dob OR :dob = 'defaultvalue!') AND
-            (Email LIKE :email OR :email = 'defaultvalue!') AND
-            (Year LIKE :studentyear OR :studentyear = 'defaultvalue!')";
-        $stmt = $db->prepare($query); //prevents SQL injection by escaping SQLite characters
-        $stmt->bindParam(':studentyear', $studentyear, SQLITE3_INTEGER);
-        $stmt->bindParam(':fname', $fname, SQLITE3_TEXT);
-        $stmt->bindParam(':lname', $lname, SQLITE3_TEXT);
-        $stmt->bindParam(':dob', $dob, SQLITE3_TEXT);
-        $stmt->bindParam(':email', $email, SQLITE3_TEXT);
-        $results = $stmt->execute();
+    if($acctype=="Student") {
+        $results=usersearchstudent($db,$studentyear,$fname,$lname,$dob,$email);
     }
-    else if ($acctype == "Faculty") {
-        //send back faculty type search results
-
-        $query = "SELECT * FROM User WHERE AccType=2 AND 
-            (Fname LIKE :fname OR :fname = 'defaultvalue!') AND
-            (Lname LIKE :lname OR :lname = 'defaultvalue!') AND
-            (DOB LIKE :dob OR :dob = 'defaultvalue!') AND
-            (Email LIKE :email OR :email = 'defaultvalue!') AND
-            (Rank LIKE :facultyrank OR :facultyrank = 'defaultvalue!')";
-        $stmt = $db->prepare($query); //prevents SQL injection by escaping SQLite characters
-        $stmt->bindParam(':facultyrank', $facultyrank, SQLITE3_TEXT);
-        $stmt->bindParam(':fname', $fname, SQLITE3_TEXT);
-        $stmt->bindParam(':lname', $lname, SQLITE3_TEXT);
-        $stmt->bindParam(':dob', $dob, SQLITE3_TEXT);
-        $stmt->bindParam(':email', $email, SQLITE3_TEXT);
-        $results = $stmt->execute();
+    elseif($acctype=="Faculty"){
+        $results=usersearchfaculty($db,$facultyrank,$fname,$lname,$dob,$email);
     }
-    else {
-        //send back a general search (may change to exclude admins)
-
-        $query = "SELECT * FROM User WHERE
-            (Fname LIKE :fname OR :fname = 'defaultvalue!') AND
-            (Lname LIKE :lname OR :lname = 'defaultvalue!') AND
-            (DOB LIKE :dob OR :dob = 'defaultvalue!') AND
-            (Email LIKE :email OR :email = 'defaultvalue!') AND
-            (Rank LIKE :facultyrank OR :facultyrank = 'defaultvalue!')";
-        $stmt = $db->prepare($query); //prevents SQL injection by escaping SQLite characters
-        $stmt->bindParam(':fname', $fname, SQLITE3_TEXT);
-        $stmt->bindParam(':lname', $lname, SQLITE3_TEXT);
-        $stmt->bindParam(':dob', $dob, SQLITE3_TEXT);
-        $stmt->bindParam(':email', $email, SQLITE3_TEXT);
-        $stmt->bindParam(':facultyrank', $facultyrank, SQLITE3_TEXT);
-        $results = $stmt->execute();
+    else{
+        $results = gensearch($db,$fname,$lname,$dob,$email,$facultyrank);
     }
+
 
     global $jsonArray;
 
