71505f2e31ec7378cbbfb5e3c3db4773db3c6a3d
Jonathan Parker <jparker3@augusta.edu>
2022-02-18 22:42:35 -0500

    OOP conversion

--- app/src/DBController.php
+++ app/src/DBController.php
@@ -4,29 +4,6 @@
     global $db;
     $db = new SQLite3($GLOBALS['dbPath'], $flags = SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE, $encryptionKey = "");
 
-    function ValidateUser($un,$pw)
-    {
-    //convert password to 80 byte hash using ripemd256 before comparing
-     $hashpassword = hash('ripemd256', $pw); 
- 
-     $myusername = strtolower($un); //makes username noncase-sensitive
-
-    //query for count
-     $query = "SELECT COUNT(*) as count FROM User WHERE Email='$myusername' AND Password='$hashpassword'";
-     $count = $GLOBALS['db']->querySingle($query);
-
-    //query for the row(s)
-     $query = "SELECT * FROM User WHERE Email='$myusername' AND Password='$hashpassword'";
-     $results = $GLOBALS['db']->query($query);
-
-     if ($results != false AND ($userinfo = $results->fetchArray()) != (null OR false))
-     {
-        $GLOBALS['acctype'] = $userinfo[2];
-         return true;
-     }
-
-     else return false;
-    }
     function usersearchstudent($db,$studentyear,$fname,$lname,$dob,$email)
     {
          {
--- app/src/LoginController.php
+++ app/src/LoginController.php
@@ -1,21 +1,55 @@
 <?php
-try {
-    /*Get DB connection*/
-    require_once "../src/DBController.php";
+require "../src/DBConnector.php";
+require "../src/User.php";
 
-    
-    function DisplayError($errorCode)
+global $acctype;
+
+class LoginController
+{
+
+    public static function Login($un,$pw)
     {
-        if($errorCode==1)
+        if (LoginController::ValidateInput($un,$pw)==true) //Validate Input
         {
-            echo "Username or password is incorrect, please try again.";
+            $User = DBConnector::GetUser($un); //GetUser() -> User
+
+            if (LoginController::ValidateUser($un,$User->GetEmail(),$pw,$User->GetPassword()==true))
+            {
+                $acctype = $User->GetAccType();
+
+                if (isset($_SESSION))
+                {
+                    //a session already existed
+                    session_destroy();
+                    session_start();
+                    $_SESSION['email'] = $un;
+                    $_SESSION['acctype'] = $acctype;
+                } 
+                
+                else
+                {
+                    //a session did not exist
+                    session_start();
+                    $_SESSION['email'] = $un;
+                    $_SESSION['acctype'] = $acctype;
+                }
+
+                //redirect
+                header("Location: ../public/dashboard.php");
+            }
+
+            else
+            {
+                header("Location: ../public/LoginForm.php?login=fail");
+            }
+        }
+
+        else
+        {
+            header("Location: ../public/LoginForm.php?login=fail");
         }
     }
 
-    //consolidated method to validate username and password
-    //password must be between 8 and 20 characters
-    //and must contain at least one upper case, lower case,
-    //number, and symbol !@#$%^&*()
     function ValidateInput($un,$pw)
     {
         $passwordFormat = "/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()]).{8,20}/";
@@ -28,63 +62,16 @@ try {
         else return false;
     }
 
-    if(ValidateInput($_POST['username'],$_POST['password']))
+    function ValidateUser($un,$userUname,$pw,$userPword)
     {
-        $myusername = htmlentities($_POST['username']);
-        $mypassword = htmlentities($_POST['password']);
-    }
+        $inputUname = strtolower($un); //makes username noncase-sensitive
+        $hashedInputPword = hash('ripemd256', $pw);
 
-    if($mypassword==null||$myusername==null)
-    {
-        $errorCode = 1;
-        DisplayError($errorCode);
-    }
-
-    global $acctype;
-    
-    if (ValidateUser($myusername,$mypassword)) // user found
-    {
-        $error = false;
-
-        if (isset($_SESSION)) 
+        if ($inputUname == $userUname && $hashedInputPword == $userPword)
         {
-            //a session already existed
-            session_destroy();
-            session_start();
-            $_SESSION['email'] = $myusername;
-            $_SESSION['acctype'] = $acctype;
-        } 
-        else 
-        {
-            //a session did not exist
-            session_start();
-            $_SESSION['email'] = $myusername;
-            $_SESSION['acctype'] = $acctype;
+            return true;
         }
-        //redirect
-        header("Location: ../public/dashboard.php");
+
+        else return false;
     }
-
-    else // user not found
-    {
-        $error = true;
-
-        //login fail
-        header("Location: ../public/LoginForm.php?login=fail");
-        //note: since the database is not changed, it is not backed up
-    }
-
- }
- catch(Exception $e)
- {
-     //prepare page for content
-     include_once "ErrorHeader.php";
- 
-     //Display error information
-     echo 'Caught exception: ',  $e->getMessage(), "<br>";
-     var_dump($e->getTraceAsString());
-     echo 'in '.'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']."<br>";
- 
-     $allVars = get_defined_vars();
-     debug_zval_dump($allVars);
- }
+}
