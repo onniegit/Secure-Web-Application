2db5ee6a1035849357413e7f43d15513401371dd
Alex Hall <ahall2@augusta.edu>
2022-03-04 18:31:16 -0500

    Server configuration

--- startup.bat
+++ startup.bat
@@ -3,4 +3,4 @@ REM Starts the webserver.
 START cmd /k CALL webserv.bat
 REM Opens browser and points to the localhost after a couple of seconds.
 SET WAIT_TIME=2
-START http://localhost:8000/public/index.php
+START https://localhost:443/public/index.php
