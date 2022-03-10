aa61a506567eb24f892a9888bab1a9a56c65e3bb
Alex Hall <ahall2@augusta.edu>
2022-03-10 05:33:55 -0500

    Modified batch files Deleted unneccesary file cgi.bat Startup.bat waits for 2 seconds before starting browser. Added instructions for quitting the server in webserv.bat. php-cgi.exe opens in a new window. nginx server quits after closing php-cgi.

--- app/bin/webserv.bat
+++ app/bin/webserv.bat
@@ -1,4 +1,6 @@
-REM ECHO OFF
+REM To quit the server, first close the window for php-cgi
+REM When prompted to terminate batch job, select no
+ECHO OFF
 REM This starts up the database and the server
 cd nginx
 REM Starts the nginx server
@@ -8,3 +10,9 @@ REM Sets temporary path for libs
 SET PATH=php.exe
 REM Populates initial database
 php bin/nginx/www/src/startup.php
+REM Allows for running php script in nginx
+start /wait /min php-cgi.exe -b 127.0.0.1:9000
+cd bin\nginx
+REM Quits the nginx server after the cgi is closed
+nginx -s quit
+REM Note that the web application will not work unless the server and cgi processes are running
--- startup.bat
+++ startup.bat
@@ -1,7 +1,7 @@
+ECHO OFF
 cd "%~dp0"\app\bin
 REM Starts the webserver.
 START cmd /c CALL webserv.bat
-START cmd /k CALL cgi.bat
 REM Opens browser and points to the localhost after a couple of seconds.
-SET WAIT_TIME=2
+TIMEOUT /t 2 /nobreak
 START https://localhost:443/public/index.php
