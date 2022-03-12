41e8832c0cb8453ac08c6aa3abb37cf5b00f647b
namingwrongs <97925903+namingwrongs@users.noreply.github.com>
2022-03-11 19:25:26 -0500

    Fix paths for new directory locations

--- app/ext/index.php
+++ app/ext/index.php
@@ -1,18 +1,18 @@
 <?php
 try {
-  require_once "../src/RedirectController.php";
+  require_once "../bin/nginx/www/src/RedirectController.php";
 
   if ($GLOBALS['rc']->ValidateLogin()) 
   {
     //redirect to dashboard
-    header("Location: ../public/dashboard.php");
+    header("Location: ../bin/nginx/www/public/dashboard.php");
   } else {
       
     //redirect to login
-    header("Location: ../public/LoginForm.php");
+    header("Location: ../bin/nginx/www/public/LoginForm.php");
   }
 }
 catch(Exception $e)
 {
-    header("Location: ../public/index.php?login=fail");
+    header("Location: ../bin/nginx/www/public/index.php?login=fail");
 }
