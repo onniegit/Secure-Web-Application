f5fbfb8e3ca79bb0f64e185803e6f87af3a16cd5
Alex Hall <ahall2@augusta.edu>
2022-03-17 02:41:57 -0400

    Server now listens on less common ports 8000 and 44343

--- startup.bat
+++ startup.bat
@@ -4,4 +4,4 @@ REM Starts the webserver.
 START cmd /c CALL webserv.bat
 REM Opens browser and points to the localhost after a couple of seconds.
 TIMEOUT /t 2 /nobreak
-START https://localhost:443/public/index.php
+START https://localhost:44343/public/index.php
