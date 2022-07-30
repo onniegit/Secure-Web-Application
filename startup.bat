63c9ccc4d816b6d9553a079fbcea43b866deca1e
Onyeka Ezenwoye <84094620+onniegit@users.noreply.github.com>
2022-07-30 15:16:52 -0400

    Add files via upload

--- startup.bat
+++ startup.bat
@@ -1,7 +1,10 @@
 ECHO OFF
 cd "%~dp0"\app\bin
-REM Starts the webserver.
+REM Starts the webserver and initialze the DB.
 START cmd /c CALL webserv.bat
 REM Opens browser and points to the localhost after a couple of seconds.
+REM Port 44343 is used for secure http.
 TIMEOUT /t 2
-START https://localhost:44343/public/index.php
+
+REM display the login page
+START https://localhost:44343/public/LoginForm.php
