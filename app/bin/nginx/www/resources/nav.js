194503b152d5fb41d94a78fd084064ca0429539a
Onyeka Ezenwoye <84094620+onniegit@users.noreply.github.com>
2022-07-30 15:24:10 -0400

    Add files via upload

--- app/bin/nginx/www/resources/nav.js
+++ app/bin/nginx/www/resources/nav.js
@@ -2,8 +2,8 @@ function toDashboard (){
     location.href = 'dashboard.php'; //only found in other public files
 }
 
-function toLogout (){
-    location.href = '../src/Logout.php'; //only found in other public files
+function Logout (){
+    location.href = '../public/dashboard.php?logout';
 }
 
 function toIndex (){
