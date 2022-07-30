7a3ada4ba9e6bf0eeabf0d92db821e0e2ae36b96
Onyeka Ezenwoye <84094620+onniegit@users.noreply.github.com>
2022-07-30 15:27:14 -0400

    Add files via upload

---
+++ app/bin/nginx/db/index.php
@@ -0,0 +1,18 @@
+<?php
+try {
+  require_once "../src/RedirectController.php";
+
+  if ($GLOBALS['redirectController']->ValidateLogin()) 
+  {
+    //redirect to dashboard
+    header("Location: ../public/dashboard.php");
+  } else {
+      
+    //redirect to login
+    header("Location: ../public/LoginForm.php");
+  }
+}
+catch(Exception $e)
+{
+    header("Location: ../public/index.php?login=fail");
+}
