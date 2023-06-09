2583562e6aadf69bd3ff19a64afe6f9b98950929
Onyeka Ezenwoye <84094620+onniegit@users.noreply.github.com>
2023-06-09 17:32:04 -0400

    Add files via upload

--- app/bin/nginx/www/resources/usersearchdisplay.js
+++ app/bin/nginx/www/resources/usersearchdisplay.js
@@ -1,28 +1,15 @@
 function fetch() {
-    // (A) GET SEARCH TERMS
-    var data = new FormData();
-    data.append('acctype', document.getElementById("acctype").value);
-    data.append('fname', document.getElementById("fname").value);
-    data.append('lname', document.getElementById("lname").value);
-    data.append('dob', document.getElementById("dob").value);
-    data.append('email', document.getElementById("email").value);
-    //check if student or faculty was selected
-    if(document.getElementById("acctype").value === "Student")
-    {
-        data.append('studentyear', document.getElementById("studentyear").value);
-    }
-    else
-    {
-        data.append('facultyrank', document.getElementById("facultyrank").value);
-    }
-
+    
     // (B) AJAX SEARCH REQUEST
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', "../src/UserSearchLogic.php", true);
-    xhr.onload = function () {
+
+    xhr.open('GET', "../src/UserSearchLogic.php", true);
+    
+    xhr.onload = function () 
+    {
         //there must be no other echos except the JSON file or JSON.parse fails
         var results = JSON.parse(this.response),
-            wrapper = document.getElementById("results");
+        wrapper = document.getElementById("results");
         try
         {
             while (wrapper.removeChild(wrapper.childNodes[0]) !== null)
@@ -35,15 +22,17 @@ function fetch() {
             //succeeds when it throws exception
         }
 
-            if (results !== null) //using results.length crashed when there was no search results
+            if (results != null) //using results.length crashed when there was no search results
             {
                 wrapper.innerHTML = "";
                 var fixedDOB = "";
                 var DOB = "";
                 var fixedStudentYear;
-                for (let res of results) {
+                for (let res of results) 
+                {
                     let row = document.createElement("span");
-                    if (res['AccType'] === 3) {
+                    if (res['AccType'] === 3) 
+                    {
                         if(res['DOB'] !== "") //make sure DOB isn't empty or it will crash
                         {
                             fixedDOB = dateFromUTC(res['DOB'], '-'); //create a Date object using SQLite's format
@@ -54,7 +43,7 @@ function fetch() {
                             DOB = "";
                         }
                         fixedStudentYear = fixStudentYear(res['Year']);
-                        row.innerHTML = `<form method="post" action="edit_account.php"><table class="search_table"><tr>
+                        row.innerHTML = `<form method="post" action="UserSearchForm.php"><table class="search_table"><tr>
                                          <td class="search_results_output">${res['LName']}, ${res['FName']}</td> 
                                          <td class="search_results_output">${DOB}</td>
                                          <td class="search_results_output" id="email"><input type="hidden" value="${res['Email']}" name="email">${res['Email']}</input></td> 
@@ -63,7 +52,7 @@ function fetch() {
                                          </tr></table></form>`;
                     }
                     else
-                        {
+                    {
                             if(res['DOB'] !== "") //make sure DOB isn't empty or it will crash
                             {
                                 fixedDOB = dateFromUTC(res['DOB'], '-'); //create a Date object using SQLite's format
@@ -73,28 +62,43 @@ function fetch() {
                             {
                                 DOB = "";
                             }
-                        row.innerHTML = `<form method="post" action="edit_account.php"><table class="search_table"><tr>
+                        row.innerHTML = `<form method="post" action="UserSearchForm.php"><table class="search_table"><tr>
                                          <td class="search_results_output">${res['LName']}, ${res['FName']}</td> 
                                          <td class="search_results_output">${DOB}</td>
                                          <td class="search_results_output" id="email"><input type="hidden" value="${res['Email']}" name="email">${res['Email']}</input></td> 
                                          <td class="search_results_output">${res['Rank']}</td>
                                          <td class="search_results_output"><button name="Edit" id="Edit" type="submit">Edit</button></td>
                                          </tr></table></form>`;
-                        }
+                    }
 
                     wrapper.appendChild(row);
                 }
             }
             else
-                {
-                wrapper.innerHTML = "No results found";
-                }
+            {
+               wrapper.innerHTML = "No results found";
+            }
     };
-    xhr.send(data);
+
+    //xhr.send(data);
+    try {
+        xhr.send();
+        if (xhr.status == 200) {
+          alert(xhr.response);
+        }
+      } catch(err) { // instead of onerror
+        
+        alert("Request failed");
+      }
+
     xhr.onloadend = function() {
         if(xhr.status === 404)
             throw new Error(' replied 404');
     }
+    xhr.onerror = function() {
+        alert("Request failed");
+    };
+      
     return false;
 }
 
@@ -136,3 +140,5 @@ function fixStudentYear(studentYearAsString)
         return "Senior";
     }
 }
+
+fetch();
